f063d2b3b131b4ed4181c448f5ad8427d3acfce2
Vladislav <M1LkMenIO@yandex.ru>
2025-04-29 21:01:39 +0300

    HM-4.4. fix

--- src/main/java/pro/sky/hogwarts/controller/InfoController.java
+++ src/main/java/pro/sky/hogwarts/controller/InfoController.java
@@ -2,16 +2,15 @@ package pro.sky.hogwarts.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("getPort")
 public class InfoController {
-    @Value("${server.port}")
-    String port;
 
-    @GetMapping("")
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/port")
     public String getPort() {
         return port;
     }
